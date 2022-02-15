c5fc42a0219ad15c17770bd602a6a6085be08635
TerryphuaCH <s10219485@connect.np.edu.sg>
2022-02-15 14:51:20 +0800

    javascript logi

--- script.js
+++ script.js
@@ -401,3 +401,25 @@ window.onload = function () {
     window.scrollTo(0, 1);
   }, 0);
 };
+
+var secret = "jXPy3KLCdvj0hHPderFtC0816aU4iJbmNYg1Rb5e";
+var database = "webapp-6b505-default-rtdb.asia-southeast1";
+var url = `https://${database}.firebasedatabase.app/users.json?auth=${secret}`;
+
+function authenticateUser(that) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status == 200) {
+            var userData = JSON.parse(xhr.responseText);
+            var username = that.username.value;
+            if (username in userData && userData[username] == that.password.value) return alert("Login Successful!");
+            else return alert("Invalid username/password combination!");
+        }
+    };
+
+    xhr.send();
+}
