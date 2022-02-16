664f02bc2548daf89ced7c330807110b3b8666ae
TerryphuaCH <s10219485@connect.np.edu.sg>
2022-02-16 12:29:02 +0800

    chanegd text font

--- dailygame.js
+++ dailygame.js
@@ -15,31 +15,6 @@ function close(){
 }
 //END OF HAMBURGER MENU
 
-var i=0,text;
-  text = "delve into Singapore, fall in love with the city again"
-
-  function typing() {
-    if(i<text.length){
-      document.getElementById("text").innerHTML += text.charAt(i);
-      i++;
-      setTimeout(typing,80);
-    }
-  }
-  typing();
-
-
-const root = document.documentElement;
-const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
-const marqueeContent = document.querySelector("ul.marquee-content");
-
-root.style.setProperty("--marquee-elements", marqueeContent.children.length);
-
-for(let i=0; i<marqueeElementsDisplayed; i++) {
-  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
-}
-
-
-
 shuffle = function (o) {
   for (
     var j, x, i = o.length;
